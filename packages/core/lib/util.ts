5fc76a27fd0986fb38841775e6551c6e7e96ab9a
imeepos <1037483576@qq.com>
2019-11-23 19:16:29 +0800

    update

--- packages/core/lib/util.ts
+++ packages/core/lib/util.ts
@@ -1,4 +1,4 @@
-import "reflect-metadata"
+import "reflect-metadata";
 export const getDesignType = (target: any, propertyKey: PropertyKey) => Reflect.getMetadata('design:type', target, propertyKey as any);
 export const getDesignParamTypes = (target: any, propertyKey?: PropertyKey) => Reflect.getMetadata('design:paramtypes', target, propertyKey as any);
 export const getDesignTargetParams = (target: any) => Reflect.getMetadata('design:paramtypes', target);
